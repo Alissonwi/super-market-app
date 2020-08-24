e37f2d105c693270d4c83b93748232528de1af37
Alissonwi <31462011+Alissonwi@users.noreply.github.com>
2020-08-24 08:01:41 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,9 +5,9 @@
 
 export const environment = {
   production: false,
-  apiUrl: "http://super-market-api-env.eba-dcpkqafd.us-east-2.elasticbeanstalk.com",
-  bucketName: 'elasticbeanstalk-us-east-2-160130800063',
-  accessKeyId: 'AKIAJZWGIPXCW3SE4Q2Q',
-  secretAccessKey: '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',
-  region: 'us-east-2'
+  apiUrl: "",
+  bucketName: '',
+  accessKeyId: '',
+  secretAccessKey: '',
+  region: ''
 };
